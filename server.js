d63a12fc0cca6b3e50e4bbd89ede571eb2e945ab
안피곤 <markan82@gmail.com>
2018-08-12 01:07:10 +0900

    steemit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+
+app.use(express.static('./'));
+
+const server = app.listen(3000, function(){
+  console.log("Express server has started on port 3000")
+});
