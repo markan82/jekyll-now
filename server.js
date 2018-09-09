5c14a0be49aeb687529991feb94084035cf56d3f
안피곤 <markan82@gmail.com>
2018-09-09 12:08:34 +0900

    .

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('./'));
 
-const server = app.listen(3001, function(){
+const server = app.listen(3000, function(){
   console.log("Express server has started on port 3000")
 });
