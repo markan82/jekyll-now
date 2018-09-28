8b0180b8ac5bbb8bffb9b708f90a2694f19789ab
ojs-nexg <markan@nexg.net>
2018-09-28 10:28:34 +0900

    .

--- esteem/service-worker.js
+++ esteem/service-worker.js
@@ -1,1 +1,160 @@
-"use strict";var precacheConfig=[["index.html","4237956f24d0a6212a97b139b26e75dc"],["service-worker.js","f458add28d3266ed80c191059e6b6d27"],["static/css/app.7d79507bcf13ed0776398d5f95818d16.css","579f8fa2ed483e519d65d09ba1818611"],["static/js/0.960e7bd8c2975719bcd1.js","f624cdde46b84872590b6ce4dd441b37"],["static/js/1.5cbb606a0461b00c96a6.js","cb95fecafffba6d1f132e6db8a38aac9"],["static/js/2.31861e4088c75ad740dd.js","b9155279972057ca3e291ac54a573e0a"],["static/js/3.e566e41f9c61572a6adc.js","28a4a61d6d5118373fa72913a20a9576"],["static/js/4.a167ae0fc06ff6786615.js","ec878c6f8485bee867a8eded36670003"],["static/js/5.9b07bd4e68da6e344d8e.js","8381c8078d1a1b66ab204847bad52d5b"],["static/js/app.17d6053403586fb1cf4a.js","82bf5d72c87b68bc1c3b9e32f9d78929"],["static/js/manifest.81d3ad20ee6e68658fde.js","62f137eedf37bf734374063f76b86432"],["static/js/vendor.22f96db10384dbcdb401.js","5c8c8e76a91dcb9db8432d1e57418dc4"],["static/style.css","d41d8cd98f00b204e9800998ecf8427e"]],cacheName="sw-precache-v3-Steemlog-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,t){var n=new URL(e);return n.hash="",n.search=n.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),n.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,!1);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(n){if(!t.has(n)){var r=new Request(n,{credentials:"same-origin"});return fetch(r).then(function(t){if(!t.ok)throw new Error("Request for "+n+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(n,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(n){return Promise.all(n.map(function(n){if(!t.has(n.url))return e.delete(n)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,n=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(t=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,"index.html"),t=urlsToCacheKeys.has(n));0,t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+var precacheConfig  = [
+  "",
+  "index.html",
+  "service-worker.js",
+  "css/font-awesome.css",
+  "css/ionic.app.css",
+  "css/ionic.app.min.css",
+  "css/ionic.css",
+  "css/style.css",
+  "fonts/fontawesome-webfont.eot",
+  "fonts/fontawesome-webfont.svg",
+  "fonts/fontawesome-webfont.ttf",
+  "fonts/fontawesome-webfont.woff",
+  "fonts/fontawesome-webfont.woff2",
+  "fonts/ FontAwesome.otf",
+  "fonts/ionicons.eot",
+  "fonts/ionicons.svg",
+  "fonts/ionicons.ttf",
+  "fonts/ionicons.woff",
+  "img/about.png",
+  "img/esteem.png",
+  "img/esteem_white.png",
+  "img/isimage.png",
+  "img/logo.png",
+  "img/logo_steemfest.png",
+  "img/logo_steemmonitor.png",
+  "img/noimage.png",
+  "img/nsfwimage.png",
+  "img/ollist.png",
+  "img/photo.png",
+  "img/plus.png",
+  "img/steemDeclined.png",
+  "img/steem_icon.png",
+  "img/tap_to_see_image.xcf",
+  "img/ullist.png",
+  "img/user_profile.png",
+  "img/ user_profile.xcf",
+  "js/index.js",
+  "js/lib.js",
+  "lib/blockies.js",
+  "lib/firebase.js",
+  "lib/highcharts.src.js",
+  "lib/highstock.src.js",
+  "lib/imgcache.js",
+  "lib/ion-datetime-picker.min.css",
+  "lib/ion-datetime-picker.min.js",
+  "lib/ionic-img-cache.js",
+  "lib/ marked.js",
+  "lib/moment-with-locales.min.js",
+  "lib/speakingurl.min.js",
+  "templates/gallery_images.html",
+  "templates/info.html",
+  "templates/login.html",
+  "templates/pincode.html",
+  "templates/popover.html",
+  "templates/reply.html",
+  "templates/search.html",
+  "templates/story.html"
+],
+cacheName = "sw-precache-v3-eSteem-" + (self.registration ? self.registration.scope : ""),
+ignoreUrlParametersMatching = [/^utm_/],
+addDirectoryIndex = function(e, t) {
+    var n = new URL(e);
+    return "/" === n.pathname.slice(-1) && (n.pathname += t), n.toString()
+},
+cleanResponse = function(e) {
+    return e.redirected ? ("body" in e ? Promise.resolve(e.body) : e.blob()).then(function(t) {
+        return new Response(t, {
+            headers: e.headers,
+            status: e.status,
+            statusText: e.statusText
+        })
+    }) : Promise.resolve(e)
+},
+createCacheKey = function(e, t, n, r) {
+    var a = new URL(e);
+    return r && a.pathname.match(r) || (a.search += (a.search ? "&" : "") + encodeURIComponent(t) + "=" + encodeURIComponent(n)), a.toString()
+},
+isPathWhitelisted = function(e, t) {
+    if (0 === e.length) return !0;
+    var n = new URL(t).pathname;
+    return e.some(function(e) {
+        return n.match(e)
+    })
+},
+stripIgnoredUrlParameters = function(e, t) {
+    var n = new URL(e);
+    return n.hash = "", n.search = n.search.slice(1).split("&").map(function(e) {
+        return e.split("=")
+    }).filter(function(e) {
+        return t.every(function(t) {
+            return !t.test(e[0])
+        })
+    }).map(function(e) {
+        return e.join("=")
+    }).join("&"), n.toString()
+},
+hashParamName = "_sw-precache",
+urlsToCacheKeys = new Map(precacheConfig.map(function(e) {
+    var t = e[0],
+        n = e[1],
+        r = new URL(t, self.location),
+        a = createCacheKey(r, hashParamName, n, !1);
+    return [r.toString(), a]
+}));
+
+function setOfCachedUrls(e) {
+return e.keys().then(function(e) {
+    return e.map(function(e) {
+        return e.url
+    })
+}).then(function(e) {
+    return new Set(e)
+})
+}
+self.addEventListener("install", function(e) {
+e.waitUntil(caches.open(cacheName).then(function(e) {
+    return setOfCachedUrls(e).then(function(t) {
+        return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(n) {
+            if (!t.has(n)) {
+                var r = new Request(n, {
+                    credentials: "same-origin"
+                });
+                return fetch(r).then(function(t) {
+                    if (!t.ok) throw new Error("Request for " + n + " returned a response with status " + t.status);
+                    return cleanResponse(t).then(function(t) {
+                        return e.put(n, t)
+                    })
+                })
+            }
+        }))
+    })
+}).then(function() {
+    return self.skipWaiting()
+}))
+}), self.addEventListener("activate", function(e) {
+var t = new Set(urlsToCacheKeys.values());
+e.waitUntil(caches.open(cacheName).then(function(e) {
+    return e.keys().then(function(n) {
+        return Promise.all(n.map(function(n) {
+            if (!t.has(n.url)) return e.delete(n)
+        }))
+    })
+}).then(function() {
+    return self.clients.claim()
+}))
+}), self.addEventListener("fetch", function(e) {
+if ("GET" === e.request.method) {
+    var t, n = stripIgnoredUrlParameters(e.request.url, ignoreUrlParametersMatching);
+    (t = urlsToCacheKeys.has(n)) || (n = addDirectoryIndex(n, "index.html"), t = urlsToCacheKeys.has(n));
+    0, t && e.respondWith(caches.open(cacheName).then(function(e) {
+        return e.match(urlsToCacheKeys.get(n)).then(function(e) {
+            if (e) return e;
+            throw Error("The cached response that was expected is missing.")
+        })
+    }).catch(function(t) {
+        return console.warn('Couldn\'t serve response for "%s" from cache: %O', e.request.url, t), fetch(e.request)
+    }))
+}
+});
